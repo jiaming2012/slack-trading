efdf6377985ad706f4d2c7636d6ae7b54aea48c3
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2023-03-11 15:39:37 -0500

    add logs

--- src/handler/slack.go
+++ src/handler/slack.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"slack-trading/src/models"
 )
@@ -23,10 +24,14 @@ func SlackApiEventHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch msg := ev.GetType().(type) {
 	case *models.SlackVerificationRequest:
+		log.Info("msg: ", msg)
 		w.Write([]byte(msg.Challenge))
 	case *models.IncomingSlackMessage:
+		log.Info("msg: ", msg)
 		w.Write(nil)
 
 		fmt.Println("incoming message: ", msg)
+	default:
+		log.Info("msg default: ", msg)
 	}
 }
