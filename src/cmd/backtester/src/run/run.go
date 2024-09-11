88c02c402d9ab936a837aa73b0d8ceddd6e72e77
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2024-09-11 09:55:11 -0400

    backtester SPX fixes

--- src/cmd/backtester/src/run/run.go
+++ src/cmd/backtester/src/run/run.go
@@ -26,11 +26,125 @@ func positionFetcher() ([]eventmodels.TradierPositionDTO, error) {
 	panic("not implemented")
 }
 
+func Exec_Backtesterfunc(ctx context.Context, signalCh <-chan eventmodels.SignalTriggeredEvent, optionsRequestExecutor *eventmodels.ReadOptionChainRequestExecutor, config BacktesterConfig, execResultCh chan ExecResult, isDryRun bool) {
+	loc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		execResultCh <- ExecResult{
+			Err: fmt.Errorf("failed to load location: %w", err),
+		}
+		return
+	}
+
+	tradierOrderExecuter := eventmodels.NewTradierOrderExecuter(config.TradierTradesOrderURL, config.TradierTradesBearerToken, isDryRun, positionFetcher)
+
+	log.Infof("waiting for signal triggered events\n")
+
+	var allTrades []*eventmodels.BacktesterOrder
+	errCh := make(chan error)
+
+	go func() {
+		for err := range errCh {
+			log.Errorf("error: %v", err)
+		}
+	}()
+
+	for signal := range signalCh {
+		maxNoOfStrikes := config.MaxNoOfStrikes
+		minDistanceBetweenStrikes := config.MinDistanceBetweenStrikes
+		expirationsInDays := config.ExpirationsInDays
+		goEnv := config.GoEnv
+
+		log.Infof("received signal triggered event: %v", signal.Signal)
+
+		readOptionChainReq, err := eventconsumers.ProcessSignalTriggeredEvent(signal, tradierOrderExecuter, optionsRequestExecutor, config.OptionsYAML, loc, goEnv)
+		if err != nil {
+			log.Errorf("failed to process signal triggered event: %v", err)
+			continue
+		}
+
+		// todo: separate this into a function
+		// instead of finding the next friday, we can just use the expiration date from the config yaml
+		nextOptionExpDate := utils.DeriveNextFriday(signal.Timestamp)
+		isHistorical := true
+		data, err := optionsRequestExecutor.OptionsDataFetcher.FetchOptionChainDataInput(signal.Symbol, isHistorical, signal.Timestamp, signal.Timestamp, nextOptionExpDate, maxNoOfStrikes, minDistanceBetweenStrikes, expirationsInDays)
+
+		if err != nil {
+			log.Errorf("skipping event %v: failed to fetch option chain data: %v", signal, err)
+			continue
+		}
+
+		if data == nil {
+			log.Warnf("skipping event %v: failed to fetch option chain data", signal)
+			continue
+		}
+
+		if len(data.OptionContracts) == 0 {
+			log.Warnf("skipping event %v: no option chain data", signal)
+			continue
+		}
+
+		resultCh := make(chan map[string]interface{})
+
+		go optionsRequestExecutor.ServeWithParams(ctx, readOptionChainReq, *data, true, signal.Timestamp, resultCh, errCh)
+
+		// todo: metadata should be attached to each order
+		// todo: this should be refactored to mostly use the same as eventmain
+		highestEVBacktestOrder, err := services.DeriveHighestEVBacktesterOrder(ctx, resultCh, errCh, signal, tradierOrderExecuter, config.OptionsYAML, goEnv)
+		if err != nil {
+			log.Errorf("tradier executer: %v: send to market failed: %v", signal.Signal, err)
+		}
+
+		if highestEVBacktestOrder != nil {
+			log.Infof("new trade: adding highest EV backtest order: %v", *highestEVBacktestOrder)
+			allTrades = append(allTrades, highestEVBacktestOrder)
+		}
+	}
+
+	if len(allTrades) == 0 {
+		execResultCh <- ExecResult{
+			SuccessMsg: "no trades to process",
+			Err:        nil,
+		}
+		return
+	}
+
+	candlesDTO, err := services.FetchCandlesFromBacktesterOrders(config.Symbol, allTrades)
+	if err != nil {
+		execResultCh <- ExecResult{
+			Err: fmt.Errorf("failed to fetch candles: %w", err),
+		}
+		return
+	}
+
+	csvPath, err := services.ProcessBacktestTrades(config.Symbol, allTrades, candlesDTO, config.OutDir)
+	if err != nil {
+		execResultCh <- ExecResult{
+			Err: fmt.Errorf("ProcessBacktestTrades failed: %w", err),
+		}
+		return
+	}
+
+	execResultCh <- ExecResult{
+		SuccessMsg: fmt.Sprintf("processed %v backtest trades to %v", len(allTrades), csvPath),
+		Err:        nil,
+	}
+}
+
+type BacktesterConfig struct {
+	TradierTradesOrderURL     string
+	TradierTradesBearerToken  string
+	MaxNoOfStrikes            int
+	MinDistanceBetweenStrikes float64
+	ExpirationsInDays         []int
+	OptionsYAML               *eventmodels.OptionYAML
+	Symbol                    eventmodels.StockSymbol
+	OutDir                    string
+	GoEnv                     string
+}
+
 func Exec(ctx context.Context, wg *sync.WaitGroup, symbol eventmodels.StockSymbol, optionsConfig eventmodels.OptionsConfigYAML, outDir, goEnv string) ExecResult {
 	tradesAccountID := os.Getenv("TRADIER_TRADES_ACCOUNT_ID")
-	tradierTradesOrderURL := fmt.Sprintf(os.Getenv("TRADIER_TRADES_URL_TEMPLATE"), tradesAccountID)
 	brokerBearerToken := os.Getenv("TRADIER_BEARER_TOKEN")
-	tradierTradesBearerToken := os.Getenv("TRADIER_TRADES_BEARER_TOKEN")
 	eventStoreDbURL := os.Getenv("EVENTSTOREDB_URL")
 	optionsExpirationURL := os.Getenv("OPTION_EXPIRATIONS_URL")
 	optionChainURL := os.Getenv("OPTION_CHAIN_URL")
@@ -43,7 +157,6 @@ func Exec(ctx context.Context, wg *sync.WaitGroup, symbol eventmodels.StockSymbo
 	}
 
 	stockQuotesURL := os.Getenv("STOCK_QUOTES_URL")
-	maxNoOfStrikes := optionConfig.MaxNoOfStrikes
 
 	if optionConfig.MinDistanceBetweenStrikes == nil {
 		return ExecResult{
@@ -51,8 +164,17 @@ func Exec(ctx context.Context, wg *sync.WaitGroup, symbol eventmodels.StockSymbo
 		}
 	}
 
-	minDistanceBetweenStrikes := *optionConfig.MinDistanceBetweenStrikes
-	expirationsInDays := optionConfig.ExpirationsInDays
+	backtesterConfig := BacktesterConfig{
+		TradierTradesOrderURL:     fmt.Sprintf(os.Getenv("TRADIER_TRADES_URL_TEMPLATE"), tradesAccountID),
+		TradierTradesBearerToken:  os.Getenv("TRADIER_TRADES_BEARER_TOKEN"),
+		MaxNoOfStrikes:            optionConfig.MaxNoOfStrikes,
+		MinDistanceBetweenStrikes: *optionConfig.MinDistanceBetweenStrikes,
+		ExpirationsInDays:         optionConfig.ExpirationsInDays,
+		OptionsYAML:               optionConfig,
+		Symbol:                    symbol,
+		OutDir:                    outDir,
+		GoEnv:                     goEnv,
+	}
 
 	log.Infof("esdb url: %v", eventStoreDbURL)
 
@@ -75,97 +197,7 @@ func Exec(ctx context.Context, wg *sync.WaitGroup, symbol eventmodels.StockSymbo
 
 	resultCh := make(chan ExecResult)
 
-	go func(signalCh <-chan eventmodels.SignalTriggeredEvent, optionsRequestExecutor *eventmodels.ReadOptionChainRequestExecutor, config eventmodels.OptionsConfigYAML, errCh chan ExecResult, DryRun bool) {
-		loc, err := time.LoadLocation("America/New_York")
-		if err != nil {
-			resultCh <- ExecResult{
-				Err: fmt.Errorf("failed to load location: %w", err),
-			}
-			return
-		}
-
-		tradierOrderExecuter := eventmodels.NewTradierOrderExecuter(tradierTradesOrderURL, tradierTradesBearerToken, isDryRun, positionFetcher)
-
-		log.Infof("waiting for signal triggered events\n")
-
-		var allTrades []*eventmodels.BacktesterOrder
-		for signal := range signalCh {
-			log.Infof("received signal triggered event: %v", signal.Signal)
-
-			readOptionChainReq, err := eventconsumers.ProcessSignalTriggeredEvent(signal, tradierOrderExecuter, optionsRequestExecutor, config, loc, goEnv)
-			if err != nil {
-				log.Errorf("failed to process signal triggered event: %v", err)
-				continue
-			}
-
-			resultCh := make(chan map[string]interface{})
-			errCh := make(chan error)
-
-			// todo: separate this into a function
-			// instead of finding the next friday, we can just use the expiration date from the config yaml
-			nextOptionExpDate := utils.DeriveNextFriday(signal.Timestamp)
-			isHistorical := true
-			data, err := optionsRequestExecutor.OptionsDataFetcher.FetchOptionChainDataInput(signal.Symbol, isHistorical, signal.Timestamp, signal.Timestamp, nextOptionExpDate, maxNoOfStrikes, minDistanceBetweenStrikes, expirationsInDays)
-
-			if err != nil {
-				log.Errorf("skipping event %v: failed to fetch option chain data: %v", signal, err)
-				continue
-			}
-
-			if data == nil {
-				log.Warnf("skipping event %v: failed to fetch option chain data", signal)
-				continue
-			}
-
-			if len(data.OptionContracts) == 0 {
-				log.Warnf("skipping event %v: no option chain data", signal)
-				continue
-			}
-
-			go optionsRequestExecutor.ServeWithParams(ctx, readOptionChainReq, *data, true, signal.Timestamp, resultCh, errCh)
-
-			// todo: metadata should be attached to each order
-			// todo: this should be refactored to mostly use the same as eventmain
-			highestEVBacktestOrder, err := services.DeriveHighestEVBacktesterOrder(ctx, resultCh, errCh, signal, tradierOrderExecuter, optionConfig, goEnv)
-			if err != nil {
-				log.Errorf("tradier executer: %v: send to market failed: %v", signal.Signal, err)
-			}
-
-			if highestEVBacktestOrder != nil {
-				log.Infof("new trade: adding highest EV backtest order: %v", *highestEVBacktestOrder)
-				allTrades = append(allTrades, highestEVBacktestOrder)
-			}
-		}
-
-		if len(allTrades) == 0 {
-			resultCh <- ExecResult{
-				SuccessMsg: "no trades to process",
-				Err:        nil,
-			}
-			return
-		}
-
-		candlesDTO, err := services.FetchCandlesFromBacktesterOrders(symbol, allTrades)
-		if err != nil {
-			resultCh <- ExecResult{
-				Err: fmt.Errorf("failed to fetch candles: %w", err),
-			}
-			return
-		}
-
-		csvPath, err := services.ProcessBacktestTrades(symbol, allTrades, candlesDTO, outDir)
-		if err != nil {
-			resultCh <- ExecResult{
-				Err: fmt.Errorf("ProcessBacktestTrades failed: %w", err),
-			}
-			return
-		}
-
-		resultCh <- ExecResult{
-			SuccessMsg: fmt.Sprintf("processed %v backtest trades to %v", len(allTrades), csvPath),
-			Err:        nil,
-		}
-	}(trackerV3OptionEVConsumer.GetSignalTriggeredCh(), optionChainRequestExector, optionsConfig, resultCh, isDryRun)
+	go Exec_Backtesterfunc(ctx, trackerV3OptionEVConsumer.GetSignalTriggeredCh(), optionChainRequestExector, backtesterConfig, resultCh, isDryRun)
 
 	trackerV3OptionEVConsumer.Replay(ctx)
 
