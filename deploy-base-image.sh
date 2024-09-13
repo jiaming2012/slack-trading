8efd5b7ddd3ce8cd045377ba0edce166cff6c088
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2024-09-13 07:21:49 -0400

    add deploy scripts

---
+++ deploy-base-image.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Stop the script on any command failure
+set -e
+
+# Check if the path is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: ./deploy-base-image.sh <major/minor/patch>"
+  exit 1
+fi
+
+CONFIG_FILE=${PROJECTS_DIR}/slack-trading/.bumpversion.base-image.cfg
+
+# Check if the config file exists
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Error: Config file $CONFIG_FILE not found!"
+  exit 1
+fi
+
+# Run bump2version with the provided config file and patch version bump
+bump2version patch --config-file $CONFIG_FILE
+
+# Get the current version from the Dockerfile
+VERSION=$(grep -i "version=" Dockerfile | cut -d'=' -f2 | tr -d '" ')
+
+# Build the images with the version tag
+docker build -t ewr.vultrcr.com/grodt/grodt-base-image:$VERSION -f Dockerfile .
+
+# Push the images to the Docker registry
+docker push ewr.vultrcr.com/grodt/grodt-base-image:$VERSION
+
+# Push to Github
+git push
