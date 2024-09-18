481083504429e73942485db8407e7a3450bf5812
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2024-09-18 12:30:40 -0400

    add debug

--- src/utils/polygon_fetch.go
+++ src/utils/polygon_fetch.go
@@ -15,6 +15,8 @@ func FetchRecursively[T any](url string, fetchDataFn eventmodels.FetchDataFunc[T
 		return nil, fmt.Errorf("POLYGON_API_KEY not set: %w", err)
 	}
 
+	fmt.Printf("Polygon API Key: %s\n", apiKey)
+
 	backOff := []time.Duration{1 * time.Second, 2 * time.Second, 4 * time.Second, 8 * time.Second, 16 * time.Second, 32 * time.Second, 64 * time.Second, 128 * time.Second}
 	isDone := false
 	counter := 0
