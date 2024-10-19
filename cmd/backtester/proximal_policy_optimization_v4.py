bf9ba4fd629a3463c4c63caaeeffe4c32b8b301c
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2024-10-19 09:45:54 -0400

    bug fixes

--- cmd/backtester/proximal_policy_optimization_v4.py
+++ cmd/backtester/proximal_policy_optimization_v4.py
@@ -88,7 +88,7 @@ class RenkoTradingEnv(gym.Env):
         elif self.client.position < 0 and unit_quantity > 0:
             return abs(self.client.position) * unit_quantity
         else:
-            current_candle = self.client.get('current_candle')
+            current_candle = self.client.current_candle
             current_price = current_candle['close'] if current_candle else 0
             if current_price == 0:
                 return 0
