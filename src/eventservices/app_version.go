617269e849d8a6ed4f0e7dd8987c1570a4b9c0bb
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2025-01-09 02:31:36 -0500

    Bump app version to 2.4.2 in app_version.go

--- src/eventservices/app_version.go
+++ src/eventservices/app_version.go
@@ -10,6 +10,6 @@ type AppVersion struct{}
 
 func (m *AppVersion) Serve(r *http.Request, apiRequest eventmodels.ApiRequest3, resultCh chan interface{}, errCh chan error) {
 	resultCh <- &eventmodels.AppVersionResponseDTO{
-		Version: "2.0.0",
+		Version: "$VERSION",
 	}
 }
