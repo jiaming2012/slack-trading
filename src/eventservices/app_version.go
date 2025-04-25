bd73328313cf4b2d2488669facd04810a01a0bd5
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2025-04-25 12:40:26 -0400

    Bump app version to 3.23.10 in app_version.go

--- src/eventservices/app_version.go
+++ src/eventservices/app_version.go
@@ -9,7 +9,7 @@ import (
 type AppVersion struct{}
 
 func GetAppVersion() string {
-	return "3.23.9"
+	return "3.23.10"
 }
 
 func (m *AppVersion) Serve(r *http.Request, apiRequest eventmodels.ApiRequest3, resultCh chan interface{}, errCh chan error) {
