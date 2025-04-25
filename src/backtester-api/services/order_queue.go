ea735f4d0b47676ad5e66939a4e0b076a9aed2f6
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2025-04-25 15:23:25 -0400

    'check null order'

--- src/backtester-api/services/order_queue.go
+++ src/backtester-api/services/order_queue.go
@@ -155,7 +155,7 @@ func UpdatePendingMarginOrders(dbService models.IDatabaseService) error {
 			// remove order from new orders queue
 			order := playground.PopNewOrdersQueue(newOrder.ID)
 			if order == nil {
-				err = fmt.Errorf("handleLiveOrders: failed to pop order from new orders queue: %w", e)
+				err = fmt.Errorf("handleLiveOrders: failed to pop order #%d from new orders queue", newOrder.ID)
 				joinedErr = errors.Join(joinedErr, err)
 				return joinedErr
 			}
